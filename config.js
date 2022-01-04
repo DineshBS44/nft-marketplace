5abc469cd60aff0364f054ca7aad2635b641a6a1
Dinesh B S <dineshbs444@gmail.com>
2022-01-05 01:01:34 +0530

    my-assets & creator-dashboard and a lot of testing has been done

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
-export const nftmarketaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftaddress = "0xf9559ba0F28D682F95f3Bce82475417F049541b3"
+export const nftmarketaddress = "0xA7B0D4206151c7Daf4EcB613AFBF87563bD04EaA"
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   }, [])
 
   async function loadNFTs() {
-    const provider = new ethers.providers.JsonRpcProvider()
+    const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_URL)
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems()
