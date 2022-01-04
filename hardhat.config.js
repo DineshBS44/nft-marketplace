dda5e8104abc553e1f65a0e73f218d8f24ce0912
Dinesh B S <dineshbs444@gmail.com>
2022-01-04 20:18:24 +0530

    configured hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+const fs = require('fs');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const polygonMumbaiURL = process.env.ALCHEMY_POLYGON_MUMBAI_RPC_URL;
+const polygonMainnetURL = process.env.ALCHEMY_POLYGON_MAINNET_RPC_URL;
+const privateKey = process.env.METAMASK_PRIVATE_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: { 
+      url:  polygonMumbaiURL,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: polygonMainnetURL,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
